b67d6319a50cacae73959c9c4a5b0fb4ebe82cf7
Luis Rodero-Merino <luis.rod@gmail.com>
2018-09-09 13:04:21 +0200

    Using resource instead of bracked

--- src/main/scala/catsEffectTutorial/CopyFile.scala
+++ src/main/scala/catsEffectTutorial/CopyFile.scala
@@ -15,62 +15,70 @@
  */
 package catsEffectTutorial
 
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect._
 import cats.effect.concurrent.Semaphore
 import cats.implicits._ 
 import java.io._ 
 
 object CopyFile extends IOApp {
 
-  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long, semph: Semaphore[IO]): IO[Long] =
+  def transmit(origin: InputStream, destination: OutputStream, buffer: Array[Byte], acc: Long): IO[Long] =
     for {
       _      <- IO.cancelBoundary // Cancelable at each iteration
-      _      <- semph.acquire
-      amount <- IO{ origin.read(buffer, 0, buffer.size) }.handleErrorWith(err => semph.release *> IO.raiseError(err))
-      total  <- if(amount > -1) IO { destination.write(buffer, 0, amount) }.handleErrorWith(err => semph.release *> IO.raiseError(err)) *> semph.release *> transmit(origin, destination, buffer, acc + amount, semph)
-                else semph.release *> IO.pure(acc) // End of read stream reached (by java.io.InputStream contract), nothing to write
+      amount <- IO(origin.read(buffer, 0, buffer.size))
+      total  <- if(amount > -1) IO(destination.write(buffer, 0, amount)) *> transmit(origin, destination, buffer, acc + amount)
+                else IO.pure(acc) // End of read stream reached (by java.io.InputStream contract), nothing to write
     } yield total // Returns the actual amount of bytes transmitted
 
-  def transfer(origin: InputStream, destination: OutputStream, semph: Semaphore[IO]): IO[Long] =
+  def transfer(origin: InputStream, destination: OutputStream): IO[Long] =
     for {
       buffer <- IO{ new Array[Byte](1024 * 10) } // Allocated only when the IO is evaluated
-      acc    <- transmit(origin, destination, buffer, 0L, semph)
+      acc    <- transmit(origin, destination, buffer, 0L)
     } yield acc
 
-  def close(is: InputStream): IO[Unit] =
-    IO{is.close()}.handleErrorWith(_ => IO.unit)
+  def inputStream(f: File, guard: Semaphore[IO]): Resource[IO, FileInputStream] =
+    Resource.make {
+      IO(new FileInputStream(f))
+    } { inStream => 
+      for  {
+       _ <- guard.acquire
+       _ <- IO(inStream.close())
+       _ <- guard.release
+      } yield ()
+    }
 
-  def close(os: OutputStream): IO[Unit] =
-    IO{os.close()}.handleErrorWith(_ => IO.unit)
+  def outputStream(f: File, guard: Semaphore[IO]): Resource[IO, FileOutputStream] =
+    Resource.make {
+      IO(new FileOutputStream(f))
+    } { outStream =>
+      for  {
+       _ <- guard.acquire
+       _ <- IO(outStream.close())
+       _ <- guard.release
+      } yield ()
+    }
 
-  def copy(origin: File, destination: File): IO[Long] = {
-    val inIO: IO[InputStream]   = IO{ new FileInputStream(origin) }
-    val outIO:IO[OutputStream]  = IO{ new FileOutputStream(destination) }
-    val semph:IO[Semaphore[IO]] = Semaphore[IO](1)
+  def inputOutputStreams(in: File, out: File, guard: Semaphore[IO]): Resource[IO, (InputStream, OutputStream)] =
+    for {
+      inStream  <- inputStream(in, guard)
+      outStream <- outputStream(out, guard)
+    } yield (inStream, outStream)
 
-    (inIO, outIO, semph)                 // Stage 1: Getting resources 
-      .tupled                            // From (IO[InputStream], IO[OutputStream], IO[Semaphore]) to IO[(InputStream, OutputStream, Semaphore)]
-      .bracket{
-        case (in, out, semph) =>         // Stage 2: Using resources (for copying data, in this case)
-          transfer(in, out, semph)
-      } {
-        case (in, out, semph) =>         // Stage 3: Freeing resources
-          for {
-            _ <- semph.acquire
-            _ <- (close(in), close(out))
-                   .tupled               // From (IO[Unit], IO[Unit]) to IO[(Unit, Unit)]
-            _ <- semph.release
-          } yield ()
-      }
-  }
+  def copy(origin: File, destination: File): IO[Long] = 
+    for {
+      guard       <- Semaphore[IO](1)
+      transferred <- inputOutputStreams(origin, destination, guard).use { case (in, out) => 
+                       transfer(in, out).guarantee(guard.release)
+                     }
+    } yield transferred
 
   // The 'main' function of IOApp //
   override def run(args: List[String]): IO[ExitCode] =
     for {
       _      <- if(args.length < 2) IO.raiseError(new IllegalArgumentException("Need origin and destination files"))
                 else IO.unit
-      orig   <- IO.pure(new File(args(0)))
-      dest   <- IO.pure(new File(args(1)))
+      orig = new File(args(0))
+      dest = new File(args(1))
       copied <- copy(orig, dest)
       _      <- IO{ println(s"$copied bytes copied from ${orig.getPath} to ${dest.getPath}") }
     } yield ExitCode.Success
